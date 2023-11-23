64e0ba19afda32bac41a9ccf87051eb3e93e7765
zouhang <zouhang@didiglobal.com>
2023-11-23 19:10:28 +0800

    chore: format code

--- src/code.ts
+++ src/code.ts
@@ -1,33 +1,29 @@
 import { toUnocssClass } from 'transform-to-unocss-core';
 
 figma.codegen.on('generate', async (e) => {
-	const node = e.node;
+  const node = e.node;
 
-	const cssObj = await node.getCSSAsync();
+  const cssObj = await node.getCSSAsync();
 
-	const cssCode = Object.entries(cssObj)
-		.map(([key, value]) => `${key}: ${value.replace(/\/\*.*\*\//g, '').trim()};`)
-		.join('\n');
+  const cssCode = Object.entries(cssObj)
+    .map(([key, value]) => `${key}: ${value.replace(/\/\*.*\*\//g, '').trim()};`)
+    .join('\n');
 
-	const uno = Object.entries(cssObj)
-		.map(([key, value]) => `${key}: ${value.replace(/\/\*.*\*\//g, '').trim()}`)
-		.map((i) => {
-			let origin = toUnocssClass(i, true)[0];
-			origin = origin.replace(/-(var\(.*\))/g, (_, $1) => `-[${$1.replace(/\s/g, '_')}]`);
-			return origin;
-		})
-		.join(' ');
+  const uno = Object.entries(cssObj)
+    .map(([key, value]) => `${key}: ${value.replace(/\/\*.*\*\//g, '').trim()}`)
+    .map((i) => toUnocssClass(i, true)[0].replace(/-(var\(.*\))/g, (_, $1) => `-[${$1.replace(/\s/g, '_')}]`))
+    .join(' ');
 
-	return [
-		{
-			title: 'unocss',
-			code: uno,
-			language: 'HTML'
-		},
-		{
-			title: 'css',
-			code: cssCode,
-			language: 'CSS'
-		}
-	];
+  return [
+    {
+      title: 'unocss',
+      code: uno,
+      language: 'HTML'
+    },
+    {
+      title: 'css',
+      code: cssCode,
+      language: 'CSS'
+    }
+  ];
 });
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ['src/code.ts'],
-	format: ['iife'],
-	splitting: false,
-	sourcemap: false,
-	clean: true,
-	dts: false,
-	minify: true,
-	outExtension: () => ({
-		js: `.js`
-	})
+  entry: ['src/code.ts'],
+  format: ['iife'],
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+  dts: false,
+  minify: true,
+  outExtension: () => ({
+    js: `.js`
+  })
 });

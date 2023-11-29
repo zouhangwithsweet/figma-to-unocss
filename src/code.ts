03447aa4a6f96e31fdbf5bf66c0e3a1a88620a60
zouhang <zouhang@didiglobal.com>
2023-11-29 11:54:47 +0800

    chore: update logic

--- src/code.ts
+++ src/code.ts
@@ -10,9 +10,17 @@ figma.codegen.on('generate', async (e) => {
     .join('\n');
 
   const uno = Object.entries(cssObj)
-    .map(([key, value]) => `${key}: ${value.replace(/\/\*.*\*\//g, '').trim()}`)
+    .map(
+      ([key, value]) =>
+        `${key}: ${value
+          .replace(/\/\*.*\*\//g, '')
+          .replace(/var\(--[\w-]*,\s*(.*)\)/g, (_, $1) => $1)
+          .trim()}`
+    )
     .map((i) => toUnocssClass(i, true)[0])
-    .join(' ');
+    .join(' ')
+    .replace(/border-(\d+\.\d+|\d+)/g, (_, $1) => `border-${(Number($1) * 4)}`)
+    .replace(/(p[xylrtb])-(\d+\.\d+|\d+)px/g, (_, $1, $2) => `${$1}-${$2 / 4}`);
 
   return [
     {

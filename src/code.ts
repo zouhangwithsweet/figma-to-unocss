e473becd1eface547315ff5b21bfa5d0bd96b2e9
zouhang <1102639563@qq.com>
2023-12-23 21:28:47 +0800

    chore: optimize css

--- src/code.ts
+++ src/code.ts
@@ -22,10 +22,31 @@ figma.codegen.on('generate', async (e) => {
     .replace(/(border-[xylrtb]-)(\d+\.\d+|\d+)/g, (_, $1, $2) => `${$1}${Number($2) * 4}`)
     .replace(/(p[xylrtb])-(\d+\.\d+|\d+)px/g, (_, $1, $2) => `${$1}-${$2 / 4}`);
 
+  const unoMini = raw
+    .filter(([key]) => !key.startsWith('font-family') && !key.startsWith('text-transform'))
+    .map(
+      ([key, value]) =>
+        `${key}: ${value
+          .replace(/\/\*.*\*\//g, '')
+          .replace(/var\(--[\w-]*,\s*(.*)\)/g, (_, $1) => $1)
+          .trim()}`
+    )
+    .map((i) => toUnocssClass(i, true)[0])
+    .filter((i) => !i.startsWith('font-not-italic') && !i.startsWith('lh-normal'))
+    .join(' ')
+    .replace(/border-(\d+\.\d+|\d+)/g, (_, $1) => `border-${Number($1) * 4}`)
+    .replace(/(border-[xylrtb]-)(\d+\.\d+|\d+)/g, (_, $1, $2) => `${$1}${Number($2) * 4}`)
+    .replace(/(p[xylrtb])-(\d+\.\d+|\d+)px/g, (_, $1, $2) => `${$1}-${$2 / 4}`);
+
   return [
     {
       title: 'unocss',
       code: uno,
+      language: 'HTML'
+    },
+    {
+      title: 'unocss-mini (without default properties)',
+      code: unoMini,
       language: 'HTML'
     },
     {

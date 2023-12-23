3966177a982528f12af7c930f3425b439e6751de
zouhang <1102639563@qq.com>
2023-12-23 21:23:13 +0800

    chore: format code

--- src/code.ts
+++ src/code.ts
@@ -4,12 +4,11 @@ figma.codegen.on('generate', async (e) => {
   const node = e.node;
 
   const cssObj = await node.getCSSAsync();
+  const raw = Object.entries(cssObj);
 
-  const cssCode = Object.entries(cssObj)
-    .map(([key, value]) => `${key}: ${value.replace(/\/\*.*\*\//g, '').trim()};`)
-    .join('\n');
+  const cssCode = raw.map(([key, value]) => `${key}: ${value.replace(/\/\*.*\*\//g, '').trim()};`).join('\n');
 
-  const uno = Object.entries(cssObj)
+  const uno = raw
     .map(
       ([key, value]) =>
         `${key}: ${value
@@ -19,8 +18,8 @@ figma.codegen.on('generate', async (e) => {
     )
     .map((i) => toUnocssClass(i, true)[0])
     .join(' ')
-    .replace(/border-(\d+\.\d+|\d+)/g, (_, $1) => `border-${(Number($1) * 4)}`)
-    .replace(/(border-[xylrtb]-)(\d+\.\d+|\d+)/g, (_, $1, $2) => `${$1}${(Number($2) * 4)}`)
+    .replace(/border-(\d+\.\d+|\d+)/g, (_, $1) => `border-${Number($1) * 4}`)
+    .replace(/(border-[xylrtb]-)(\d+\.\d+|\d+)/g, (_, $1, $2) => `${$1}${Number($2) * 4}`)
     .replace(/(p[xylrtb])-(\d+\.\d+|\d+)px/g, (_, $1, $2) => `${$1}-${$2 / 4}`);
 
   return [
